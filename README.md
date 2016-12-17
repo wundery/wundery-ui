e4e322b8c536e8720f72d65140a108dbbac41368
Philipp Frank <philipp.alexander.frank@gmail.com>
2016-12-17 16:19:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wundery user interface framework
+
+Welcome! This is a CSS framework used in the Wundery store backend. We opend this project in order to enable third party developers to build apps which appear in the wundery Look and feel. You can use this repo to look around or point us on bugs.
+
+## Usage
+
+Embed the ui framework:
+
+```html
+<link rel="stylesheet" href="https://ui.wundery.com/v1.5.0/ui.css" />
+```
+
+For the React people we have sweet [bindings at place](https://github.com/wundery/wundery-ui-react).
---
+++ gulpfile.babel.js
@@ -0,0 +1,105 @@
+import gulp from 'gulp';
+import util from 'gulp-util';
+import sass from 'gulp-sass';
+import connect from 'gulp-connect';
+import clean from 'gulp-clean';
+import yargs from 'yargs';
+import runSequence from 'run-sequence';
+import sourcemaps from 'gulp-sourcemaps';
+import IF from 'gulp-if';
+import rev from 'git-rev-sync';
+import concat from 'gulp-concat';
+import cors from 'cors';
+import plumber from 'gulp-plumber';
+import replace from 'gulp-replace';
+
+const args  = yargs.argv;
+const gitTag = rev.tag();
+const buildDir = 'build';
+const releaseDir = `${buildDir}/${gitTag}`;
+
+util.log(`Building ${releaseDir}`);
+
+gulp.task('build:clean', () =>
+  gulp
+    .src([releaseDir], {read: false})
+    .pipe(clean())
+);
+
+gulp.task('build:scss', () =>
+  gulp
+    .src('src/ui.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end')
+      },
+    }))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      precision: 13
+    }))
+    // Devicons does not allow to specify the font path using a variable,
+    // so we need to replace it
+    .pipe(replace(/\.\.\/fonts\/devicons/g, './fonts/devicons'))
+    .pipe(gulp.dest(releaseDir))
+);
+
+gulp.task('build:fonts', () =>
+  gulp
+    .src([
+      'node_modules/font-awesome/fonts/*',
+      'node_modules/devicons/fonts/*'
+    ])
+    .pipe(gulp.dest(`${releaseDir}/fonts`))
+);
+
+gulp.task('build:assets', () =>
+  gulp
+    .src([
+      'src/assets/**'
+    ])
+    .pipe(gulp.dest(`${releaseDir}/assets`))
+);
+
+
+gulp.task('watch:scss', () => gulp.watch('src/**/*.scss', ['build:scss']));
+
+gulp.task('watch', () => runSequence('watch:scss'));
+
+gulp.task('serve', connect.server({
+  root: [buildDir],
+  port: 8042,
+  middleware() {
+    return [
+      cors()
+    ];
+  }
+})
+);
+
+gulp.task('nulltask', () => true);
+
+gulp.task('default', () =>
+  runSequence(
+    'build:clean',
+    [
+      'build:scss',
+      'build:fonts',
+      'build:assets',
+    ],
+    'watch',
+    'serve'
+  )
+);
+
+gulp.task('build', () =>
+  runSequence(
+    'build:clean',
+    [
+      'build:scss',
+      'build:fonts',
+      'build:assets',
+    ]
+  )
+);
